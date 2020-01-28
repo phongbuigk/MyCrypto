e29beee509601254d2b94258a73637a563cd252d
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-01-28 16:03:39 +0100

    Move walletConnect into WalletService/walletconnect

--- common/v2/services/WalletService/walletconnect/index.ts
+++ common/v2/services/WalletService/walletconnect/index.ts
@@ -1,3 +1,4 @@
 export { default as WalletConnectSingleton } from './walletConnectSingleton';
 export { default as WalletConnectProvider, WalletConnectContext } from './WalletConnectProvider';
 export { default as walletConnectState } from './reducer';
+export { default as WalletConnectWallet } from './walletConnect';
